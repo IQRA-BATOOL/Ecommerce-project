5e3f60ce4e1daf6ea76a271c7664dd20e81acdb0
IQRA-BATOOL <F2021376087@umt.edu.pk>
2024-11-12 02:05:17 +0500

    Fetch Api data using Axios

--- src/context/productcontex.js
+++ src/context/productcontex.js
@@ -1,9 +1,27 @@
-
-import { createContext, useContext} from "react";
+import axios from "axios";
+import { createContext, useContext, useEffect} from "react";
 
 const AppContext = createContext();
+
+const API = "https://api.pujakaitem.com/api/products";  
 const AppProvider = ({ children }) => {
-    return <AppContext.Provider> value= {{ myname : Iqra}}{children}</AppContext.Provider>
+
+   const getProducts = async(url)=>{
+    const res = await axios.get(url);
+    const products = await res.data;
+    
+console. log (
+    " ~ file:productcontext.js ~ line 10  ~getProducts ~ res" ,
+    res
+);
+
+   };
+   useEffect(() => {  
+    getProducts(API);
+},[]);
+    return (
+        <AppContext.Provider> value= {{ myname : Iqra}}{children}</AppContext.Provider>
+    );
 };
 //coustom hook
 const useproductContext = ()=>{
