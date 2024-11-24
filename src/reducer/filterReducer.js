357dc4955ed779ae3e310e68c4ae1ee74774263e
IQRA-BATOOL <F2021376087@umt.edu.pk>
2024-11-25 01:42:18 +0500

    searchh filter in react

--- src/reducer/filterReducer.js
+++ src/reducer/filterReducer.js
@@ -26,16 +26,39 @@ const filterReducer = (state ,  action) => {
             ...state,
             sorting_view,
          };
+         case "SORTING_PRODUCTS":
+      let newSortData;
+      // let tempSortProduct = [...action.payload];
+
+      const { filter_products, sorting_value } = state;
+      let tempSortProduct = [...filter_products];
+
+      const sortingProducts = (a, b) => {
+        if (sorting_value === "lowest") {
+          return a.price - b.price;
+        }
+
+        if (sorting_value === "highest") {
+          return b.price - a.price;
+        }
+
+        if (sorting_value === "a-z") {
+          return a.name.localeCompare(b.name);
+        }
+
+        if (sorting_value === "z-a") {
+          return b.name.localeCompare(a.name);
+        }
+      };
+
+      newSortData = tempSortProduct.sort(sortingProducts);
+
+      return {
+        ...state,
+        filter_products: newSortData,
+      };
    
 
-
-
-
-
-
-    default:
-         return state
- }
 };
 
 export default filterReducer;
